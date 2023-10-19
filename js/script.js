11c1567aa2f61b8195e10ac3c4625c60b6202cbe
IIODERII <oderrisi@gmail.com>
2023-10-19 16:42:32 +0200

    added logic to bombs in script

--- js/script.js
+++ js/script.js
@@ -11,24 +11,35 @@
         const playground = document.getElementById('playground');
 
         let bombList = [] ;
+        let score;
+        let youLose;
+        let square;
+        let maxClick
 
         //bottone per iniziare il gioco
         startbtn.addEventListener('click', function() {
+            youLose = false;
+
             playground.innerHTML = '' ;
             startbtn.classList.add('d-none');
             playground.classList.remove('d-none') ;
             returntbtn.classList.remove('d-none') ;
+            document.getElementById('score').className = 'fw-bold fs-3 text-center p-3 d-inline-block mx-auto rounded-3';
 
-            const numSquare = parseInt(document.getElementById('difficulty').value);
+            const numSquare = parseInt(document.getElementById('difficulty').value);           
+            maxClick = numSquare - NUM_BOMBE ;
+
+            score = 0 ;
+            document.getElementById('score').innerHTML = `Punteggio: ${score}`;
 
             generaBombe(numSquare);
 
-            console.log(bombList);
+            console.log(bombList.sort());
             
             document.getElementById('select-container').classList.add('d-none');
 
             for(let i = 1 ; i <= numSquare ; i++){
-                let square = createSquare(i , numSquare);
+                square = createSquare(i , numSquare);
 
                 playground.append(square) ;
             }
@@ -40,6 +51,9 @@
             returntbtn.classList.add('d-none');
             playground.classList.add('d-none') ;    
             document.getElementById('select-container').classList.remove('d-none');
+            document.getElementById('score').className = 'd-none';
+
+            bombList = [] ;
         })
 
 
@@ -54,9 +68,30 @@
             square.style.height = `calc(100% /  ${squareWidth} )`;
             square.innerHTML = squareIndex ;
 
-            square.addEventListener('click' , function(){
-                square.classList.toggle('active');
-                console.log(squareIndex);
+            square.addEventListener('click' , function colorSquare(){
+
+                if(!youLose){
+                    if(bombList.includes(squareIndex)){
+                        square.classList.add('bomb');
+                        square.innerHTML = '<i class="fa-solid fa-bomb fa-beat"></i>' ;
+                        console.log('BOMBA');
+                        youLose = true ;
+                        gameOver(youLose);
+                    }else{
+                        square.classList.add('active');
+                        console.log(squareIndex);
+                        square.removeEventListener('click' , colorSquare);
+                        score++ ;
+                        document.getElementById('score').innerHTML = `Punteggio: ${score}`;
+    
+                        if(score === maxClick){
+                            gameOver(youLose);
+                        }
+                    }
+                }else{
+                    square.removeEventListener('click' , colorSquare);
+                }
+                             
             })
 
             return square;
@@ -68,13 +103,40 @@
                 if(!bombList.includes(bomba)){
                     bombList.push(bomba)
                 }
-                console.log(bomba);
             }
         }
 
         function getRndInteger(min, max) {
             return Math.floor(Math.random() * (max - min + 1) ) + min;
-          }
+        }
+
+        function gameOver(youLose){
+            const squareList = document.getElementsByClassName('square');
+
+            if(youLose && score < maxClick){
+
+                document.getElementById('score').classList.add('bg-danger');
+                document.getElementById('score').innerHTML = `
+                    Peccato hai perso. <br>
+                    Punteggio: ${score}
+                `;
+            }else{
+                document.getElementById('score').classList.add('bg-success');
+                document.getElementById('score').innerHTML = `
+                    Complimenti Hai vinto!!! <br>
+                    Punteggio: ${score}
+                `;
+            }
+
+            for(let i = 0 ; i < squareList.length ; i++){
+                let currentSquare = squareList[i] ;
+
+                if(bombList.includes(i+1)){
+                    currentSquare.classList.add('bomb');
+                    currentSquare.innerHTML = '<i class="fa-solid fa-bomb fa-beat"></i>' ;
+                }
+            }
+        }
     }
 })();
     
